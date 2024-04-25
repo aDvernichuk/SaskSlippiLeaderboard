9f8d6f456e72bda308b0be1f81d6ee0803a98586
aDvernichuk <advernichuk@gmail.com>
2024-04-25 10:22:35 +0000

    fixed cronjob

--- cron/run.sh
+++ cron/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
+export PATH="/home/ubuntu/.nvm/versions/node/v18.12.0/bin:/home/ubuntu/.nvm/versions/node/v18.12.0/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/ubuntu/.nvm/versions/node/v18.12.0/bin"
 DIR_PATH=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$DIR_PATH/.." 
 node --loader ts-node/esm --no-warnings cron/fetchStats.ts 2>&1 | tee cron/logs/log.txt
