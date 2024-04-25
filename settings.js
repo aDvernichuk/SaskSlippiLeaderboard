5ba04c40c3996f6668ba5a15de32492542edab84
aDvernichuk <advernichuk@gmail.com>
2024-04-25 08:26:55 +0000

    figured out what I did wrong

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ module.exports = {
 
   // add repo path for username.github.io/repoPath for react router to recognize paths,
   // use only when deployed to github.io, leave as null for deployments with custom domains
-  repoPath: 'https://github.com/aDvernichuk/SaskSlippiLeaderboard',
+  repoPath: 'https://advernichuk.github.io/SaskSlippiLeaderboard/',
 
   // Google sheets ID for player connect codes.
   // https://docs.google.com/spreadsheets/d/[YOUR ID]
