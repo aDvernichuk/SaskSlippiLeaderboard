49b8abb95c8be16c6db8b87882dbf7b287d9c967
aDvernichuk <advernichuk@gmail.com>
2026-02-02 14:02:39 -0600

    Updates

--- main.js
+++ main.js
@@ -2632,7 +2632,7 @@ module.exports = styleTagTransform;
 /***/ ((module) => {
 
 "use strict";
-module.exports = /*#__PURE__*/JSON.parse('{"updated":1770060621461}');
+module.exports = /*#__PURE__*/JSON.parse('{"updated":1770062419367}');
 
 /***/ }),
 
